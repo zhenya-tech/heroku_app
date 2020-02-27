319f4cf8f917d3adbcf3d4350207f7a5801fc441
ZhenyaP <zhenka.pogorelova@mail.ru>
2020-02-27 18:01:58 +0300

    update

--- app2.py
+++ app2.py
@@ -30,12 +30,11 @@ def timed_job():
     for u in users:
         round = db.get_last_round(u["id"])
         d = datetime.datetime.strptime(round[0]["time_round"], format)
-        if (datetime.datetime.now() - d > datetime.timedelta(minutes=10)) and \
-                ((u not in user_reminder) or (
-                        datetime.datetime.now() - user_reminder[u] > datetime.timedelta(minutes=5))):
-            user_reminder[u] = datetime.datetime.now()
-            viber.send_messages(u["viber_id"], [TextMessage(text="Время повторить слова", keyboard=START_KEYBOARD,
-                                                      tracking_data='tracking_data')])
+        if (datetime.datetime.now() - d > datetime.timedelta(minutes=10)):
+                if ((u not in user_reminder) or (datetime.datetime.now() - user_reminder[u] > datetime.timedelta(minutes=5))):
+                        user_reminder[u] = datetime.datetime.now()
+                        viber.send_messages(u["viber_id"], [TextMessage(text="Время повторить слова", keyboard=START_KEYBOARD,
+                                                                  tracking_data='tracking_data')])
 
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
