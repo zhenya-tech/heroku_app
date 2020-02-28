39f40d2d83e14f80ce0c1dc32c29c308dbe95a4d
ZhenyaP <zhenka.pogorelova@mail.ru>
2020-02-28 15:59:28 +0300

    update

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages import TextMessage
 import datetime
 from MyDataBase import MyDataBase
-from app import START_KEYBOARD, viber
+from app import START_KEYBOARD, viber, Session, User
 
 # bot_configuration = BotConfiguration(
 #     name='LearnEnglishBot',
@@ -24,17 +24,19 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     # print('This job is run every three minutes.')
-    db = MyDataBase('database.db')
-    users = db.get_all_users()
+    # db = MyDataBase('database.db')
+    # users = db.get_all_users()
+    session = Session()
+    users = session.query(User)
     format = "%Y-%m-%d %H:%M:%S.%f"
     for u in users:
-        round = db.get_last_round(u["id"])
-        d = datetime.datetime.strptime(round[0]["time_round"], format)
+        # round = db.get_last_round(u["id"])
+        d = datetime.datetime.strptime(u.last_time_visit, format)
         if datetime.datetime.utcnow() - d > datetime.timedelta(minutes=5):
             if ((u not in user_reminder) or (
                     datetime.datetime.utcnow() - user_reminder[u] > datetime.timedelta(minutes=3))):
                 user_reminder[u] = datetime.datetime.utcnow()
-                viber.send_messages(u["viber_id"], [TextMessage(text=f"{datetime.datetime.utcnow() - d}Время {d}повторить слова {user_reminder[u]}", keyboard=START_KEYBOARD,
+                viber.send_messages(u.viber_id, [TextMessage(text=f"{datetime.datetime.utcnow() - d}Время {d}повторить слова {user_reminder[u]}", keyboard=START_KEYBOARD,
                                                                 tracking_data='tracking_data')])
 
 
